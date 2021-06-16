5c518fb4c73188be1727dbcb30bdd1b2f5e85243
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-06-16 20:06:14 +0200

    initial commit

---
+++ src/main/java/nl/lucas/letscookapi/LetsCookApiApplication.java
@@ -0,0 +1,13 @@
+package nl.lucas.letscookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsCookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsCookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/lucas/letscookapi/LetsCookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.lucas.letscookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsCookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
