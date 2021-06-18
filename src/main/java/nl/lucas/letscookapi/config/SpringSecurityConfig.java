ded21b6141006554963011fe5623c3badf4b66a8
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-06-18 09:53:06 +0200

    added securityConfig file

---
+++ src/main/java/nl/lucas/letscookapi/config/SpringSecurityConfig.java
@@ -0,0 +1,48 @@
+package nl.lucas.letscookapi.config;
+
+import nl.lucas.letscookapi.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/recipes/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/v1/admin/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/v1/users/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/authenticated/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+
+}
